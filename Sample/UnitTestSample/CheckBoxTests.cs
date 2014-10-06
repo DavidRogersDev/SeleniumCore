091949405ae3f7120158dfad2f71073b5139e686
David Rogers <davidrogersau@yahoo.com.au>
2014-10-07 10:15:13 +1030

    Added IsElementChecked method to BaseTestDriver.

---
+++ Sample/UnitTestSample/CheckBoxTests.cs
@@ -0,0 +1,59 @@
+﻿using KesselRun.SeleniumCore.Infrastructure;
+using KesselRun.SeleniumCore.Infrastructure.Factories;
+using KesselRun.SeleniumCore.Infrastructure.Factories.Contracts;
+using KesselRun.SeleniumCore.TestDrivers.Browsers.Chrome;
+using KesselRun.SeleniumCore.TestDrivers.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using UnitTestSample.Pages;
+
+namespace UnitTestSample
+{
+    [TestClass]
+    public class CheckBoxTests
+    {
+        [TestMethod]
+        public void CheckedCheckBoxIsReportedAsNotCheckedWhenNotChecked()
+        {
+            //  Arrange
+            var driver = GetNewChromeTestDriver();
+            var bigPondSigninPage = new BigPondSignInPage(driver);
+            bigPondSigninPage.OpenPage();
+
+            //  Act
+            //  Assert                        
+            Assert.IsFalse(bigPondSigninPage.RememberMeIsChecked);
+
+            bigPondSigninPage.Quit();
+        }
+
+        [TestMethod]
+        public void CheckedCheckBoxIsReportedAsCheckedWhenChecked()
+        {
+            //  Arrange
+            var driver = GetNewChromeTestDriver();
+            var bigPondSigninPage = new BigPondSignInPage(driver);
+            bigPondSigninPage.OpenPage();
+
+            //  Act
+            bigPondSigninPage.SignIn();
+            
+            //  Assert                        
+            Assert.IsTrue(bigPondSigninPage.RememberMeIsChecked);
+
+            bigPondSigninPage.Quit();
+        }
+
+        private static ITestDriver GetNewChromeTestDriver()
+        {
+            ITestDriverFactory foundry = new TestDriverFactory(new DriverOptions
+            {
+                DriverExePath = ConfigurationManager.AppSettings["ChromeExePath"],
+                Port = int.Parse(ConfigurationManager.AppSettings["ChromeBrowserPort"])
+            });
+
+            var chromeTestDriver = foundry.CreateTestDriver<ChromeTestDriver>();
+            return chromeTestDriver;
+        }
+    }
+}
---
+++ Sample/UnitTestSample/Pages/BigPondSignInPage.cs
@@ -0,0 +1,36 @@
+﻿using KesselRun.SeleniumCore.Enums;
+using KesselRun.SeleniumCore.TestDrivers.Contracts;
+
+namespace UnitTestSample.Pages
+{
+    public class BigPondSignInPage : BasePage
+    {
+        private const string RememberMe = "rememberMe";
+
+        public BigPondSignInPage(ITestDriver seleniumTestDriver) 
+            : base(seleniumTestDriver)
+        {
+            
+        }
+
+        public void OpenPage()
+        {
+            SeleniumTestDriver.GoToUrl("https://signon.bigpond.com/login");
+        }
+
+        public void SignIn()
+        {
+            SeleniumTestDriver.FindByIdClick(RememberMe);
+        }
+
+        public bool RememberMeIsChecked
+        {
+            get { return SeleniumTestDriver.IsElementChecked(FinderStrategy.Id, RememberMe); }
+        }
+
+        public void Quit()
+        {
+            SeleniumTestDriver.Quit();
+        }
+    }
+}
